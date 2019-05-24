e6ced5d1f4cb69091998fa5f9517477114975604
Adam Spofford <pieflavor.mc@gmail.com>
2019-05-24 15:21:33 -0700

    Add support for MCMoji

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,20 @@ repositories {
     maven {
         setUrl("https://repo.spongepowered.org/maven")
     }
+    maven {
+        setUrl("https://dl.bintray.com/pie-flavor/maven")
+    }
+    maven {
+        setUrl("https://jitpack.io")
+    }
+    maven {
+        setUrl("https://repo.codemc.org/repository/maven-public/")
+    }
 }
 
 dependencies {
     compileOnly("org.spongepowered:spongeapi:7.1.0")
+    compileOnly("flavor.pie:mcmoji:1.2.0:all")
 }
 
 configure<JavaPluginConvention> {
