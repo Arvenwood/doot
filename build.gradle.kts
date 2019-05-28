b2ce79c299ec1c0b00445fe620e2eb0948222a76
Adam Spofford <pieflavor.mc@gmail.com>
2019-05-28 07:01:07 -0700

    It's on jcenter now

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,10 @@ group = "arven"
 version = "1.0.0"
 
 repositories {
+    jcenter()
     mavenCentral()
     maven {
         setUrl("https://repo.spongepowered.org/maven")
-    }
-    maven {
-        setUrl("https://dl.bintray.com/pie-flavor/maven")
     }
     maven {
         setUrl("https://jitpack.io")
